b0de466944c0b5298754a2b090588c5841362d66
aong <yxwzaxns@gmail.com>
2017-05-05 17:34:11 +0800

    233

---
+++ deploy.php
@@ -0,0 +1,41 @@
+<?php
+namespace Deployer;
+require 'recipe/laravel.php';
+
+// Configuration
+
+set('ssh_type', 'native');
+set('ssh_multiplexing', true);
+
+set('repository', 'git@github.com:yxwzaxns/position.git');
+
+add('shared_files', []);
+add('shared_dirs', []);
+
+add('writable_dirs', []);
+
+// Servers
+
+server('production', '120.24.65.9')
+    ->user('web')
+    ->identityFile()
+    ->set('deploy_path', '/var/www');
+//    ->pty(true);
+
+
+// Tasks
+
+desc('Restart PHP-FPM service');
+task('php-fpm:restart', function () {
+    // The user must have rights for restart service
+    // /etc/sudoers: username ALL=NOPASSWD:/bin/systemctl restart php-fpm.service
+    run('sudo systemctl restart php-fpm.service');
+});
+after('deploy:symlink', 'php-fpm:restart');
+
+// [Optional] if deploy fails automatically unlock.
+after('deploy:failed', 'deploy:unlock');
+
+// Migrate database before symlink new release.
+
+before('deploy:symlink', 'artisan:migrate');
