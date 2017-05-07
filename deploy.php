1c4e1d126c2a98421dbdfe3006bbb9691976bec3
aong <yxwzaxns@gmail.com>
2017-05-07 15:40:28 +0800

    2335

--- deploy.php
+++ deploy.php
@@ -9,33 +9,46 @@ set('ssh_multiplexing', true);
 
 set('repository', 'git@github.com:yxwzaxns/position.git');
 
-add('shared_files', []);
+add('shared_files', [
+    '.env',
+]);
 add('shared_dirs', []);
 
-add('writable_dirs', []);
+add('writable_dirs', [
+    'bootstrap/cache',
+    'storage',
+    'storage/app',
+    'storage/app/public',
+    'storage/framework',
+    'storage/framework/cache',
+    'storage/framework/sessions',
+    'storage/framework/views',
+    'storage/logs',
+]);
 
 // Servers
 
 server('production', '120.24.65.9')
     ->user('web')
     ->identityFile()
-    ->set('deploy_path', '/var/www');
+    ->set('deploy_path', '/home/web/poc');
 //    ->pty(true);
 
 
 // Tasks
 
-desc('Restart PHP-FPM service');
-task('php-fpm:restart', function () {
-    // The user must have rights for restart service
-    // /etc/sudoers: username ALL=NOPASSWD:/bin/systemctl restart php-fpm.service
-    run('sudo systemctl restart php-fpm.service');
-});
-after('deploy:symlink', 'php-fpm:restart');
-
-// [Optional] if deploy fails automatically unlock.
-after('deploy:failed', 'deploy:unlock');
-
-// Migrate database before symlink new release.
-
-before('deploy:symlink', 'artisan:migrate');
+//desc('Restart PHP-FPM service');
+//task('php-fpm:restart', function () {
+//    // The user must have rights for restart service
+//    // /etc/sudoers: username ALL=NOPASSWD:/bin/systemctl restart php-fpm.service
+//    run('sudo systemctl restart php-fpm.service');
+//});
+//after('deploy:symlink', 'php-fpm:restart');
+//
+//// [Optional] if deploy fails automatically unlock.
+//after('deploy:failed', 'deploy:unlock');
+//
+//// Migrate database before symlink new release.
+//
+//before('deploy:symlink', 'artisan:migrate');
+after('deploy', 'success');
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -60,6 +60,7 @@
                                 </a>
                             </div>
                         </div>
+                        <input type="hidden" name="_token"  value="{csrf_token()}"/>
                     </form>
                 </div>
             </div>
